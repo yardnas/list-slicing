758fd57095eb03323acf6a87249c8390d6a81ed1
Sandra You <syou327@yahoo.com>
2021-04-29 20:35:54 +0000

    completed custom_len function
    Co-Author: Anne Bercilla <annebercilla@gmail.com>

--- further_study.py
+++ further_study.py
@@ -16,7 +16,12 @@ def custom_len(input_list):
 
     """
 
-    return 0
+    counter = 0 
+
+    for item in input_list:
+        counter += 1
+
+    return counter
 
 
 # For the next four exercises, you'll need to be clever and think about ways
